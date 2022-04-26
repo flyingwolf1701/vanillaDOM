a0747defa545400fab667ed8b5d40878e0f655fe
Patrick Reid <patrickjreid@gmail.com>
2022-04-26 12:17:07 -0600

    initial commit dev branch

--- client/index.js
+++ client/index.js
@@ -2,6 +2,6 @@ const body = document.body;
 
 const main = document.createElement('main') 
 main.setAttribute('class', 'main-container')
-main.innerText = 'hello'
+main.innerText = 'test'
 body.append(main)
 
