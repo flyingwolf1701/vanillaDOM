4f9b8aefbc12de77cc7d26b34de941c70a4862b1
Patrick Reid <patrickjreid@gmail.com>
2022-04-26 12:14:02 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+practice building a website using just DOM manipulation. Working with an empty html page.
+
+1) index.js, style.css, index.html. attach js and css to html.
---
+++ client/index.js
@@ -0,0 +1,7 @@
+const body = document.body;
+
+const main = document.createElement('main') 
+main.setAttribute('class', 'main-container')
+main.innerText = 'hello'
+body.append(main)
+
